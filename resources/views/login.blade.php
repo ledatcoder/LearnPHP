221a656ea508cab4808cc9fe739aaa2b709e1883
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-03-07 15:12:04 +0700

    P1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row mt-5 justify-content-center">
+        <div class="col-md-6 ">
+            <h2 class="mb-4">Login</h2>
+            @if($errors -> any())
+                @foreach ($errors->all() as $error )
+                    <div class="alert alert-danger">{{$error}}</div>
+                @endforeach
+            @endif
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{route('login.submit')}}" method="POST">
+                        @csrf
+                        <div class="mb-2">
+                            <label for="" class="form-label">User Name</label>
+                            <input  name="name" type="text" class="form-control">
+                        </div>
+                        <div class="mb-2">
+                            <label for="" class="form-label">Email</label>
+                            <input name="email" type="email" class="form-control">
+                        </div> <div class="mb-2">
+                            <label for="" class="form-label">Password</label>
+                            <input name="password" type="text" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-dark">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
